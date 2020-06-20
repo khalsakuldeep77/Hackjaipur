74d8a281aad694adace2464bf3b9ebe4b7feea1a
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-20 22:55:03 +0530

    Updated Component

--- React Native/components/ImagePickerExample.js
+++ React Native/components/ImagePickerExample.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button, Image, View, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import Constants from "expo-constants";
+import * as FileSystem from "expo-file-system";
 
 const ImagePickerExample = () => {
   const [image, setImage] = useState(null);
@@ -34,10 +35,50 @@ const ImagePickerExample = () => {
     }
   };
 
+  const _takePhoto = async () => {
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!pickerResult.cancelled) {
+      setImage(pickerResult.uri);
+    }
+  };
+
+  const base64 = await FileSystem.readAsStringAsync(image, { encoding: 'base64' });
+
+  console.log(base64);
+
+  // const convertImage = () => {
+    // const base64 = await FileSystem.readAsStringAsync(image, { encoding: 'base64' });
+  // };
+
+  // const predict = () => {
+  //   var data = {
+  //     "url": base64
+  //  }
+
+  //  fetch("https://....", {
+  //     method: "POST",
+  //     headers: headers,
+  //     body:  JSON.stringify(data)
+  //  })
+  //  .then(function(response){
+  //   return response.json();
+  //  })
+  //  .then(function(data){
+  //  console.log(data)
+  //  });
+  // };
+
   return (
     <View style={styles.container}>
       {image && <Image source={{ uri: image }} style={styles.image} />}
       <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <Button title="Take photo from camera" onPress={_takePhoto} />
+      <Button title="Predict" onPress={() => {}} />
     </View>
   );
 };
