f29abb42c25fc2c60cb2bad950ba4ac4fdd41dd3
Deepak Tejwani <deepaktejwani1@gmail.com>
2020-06-20 14:36:24 +0530

    More Changes..

--- Flask/User_Auth_Flask/app.py
+++ Flask/User_Auth_Flask/app.py
@@ -18,7 +18,7 @@ auth = firebase.auth()
 email = input('Please Enter your Email\n')
 password = input('Please enter your password\n')
 
-#user = auth.create_user_with_email_and_password(email, password)
-user = auth.sign_in_with_email_and_password(email, password)
+user = auth.create_user_with_email_and_password(email, password)
+#user = auth.sign_in_with_email_and_password(email, password)
 
 auth.get_account_info(user['idToken'])
